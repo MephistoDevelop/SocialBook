ac25d6a4c43345626b656d84259d88f1965585e7
Cristian <inesherald@gmail.com>
2019-10-11 17:20:24 -0500

    add post model and migration to user

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  validates :content,presence:true,length{ maximum: 1000 }
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence:true
+  validates :email, presence:true,format:{ with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_many :posts
 end
