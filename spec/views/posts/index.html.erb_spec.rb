f2a393b32e3ae3c7b4c07eaf463b3ee78f7c2298
serdg0 <sergio.diazg92@gmail.com>
2019-10-15 16:54:26 -0500

    added post index test

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe "posts/index.html.erb", type: :view do
   it 'posts show content and author name' do
     user = User.create(username: 'Example', email: 'Example@mail.com',
                     password: '123456', password_confirmation: '123456')
-    sign_in user
-    post = Post.new(content: 'Example test', user_id: 1)
+    post = Post.create(content: 'Example test', user_id: 1)
+    visit '/posts'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Log in'
     visit '/posts'
     expect(page).to have_content('Fakebook')
-    expect(page).to have_content('Name')
-    expect(page).to have_content('Email')
+    expect(page).to have_content('User')
   end
 end
