b2313d558946648d8c30ff9c1cb23978ef6b5f6a
Cristian <inesherald@gmail.com>
2019-10-16 11:49:39 -0500

    fix factorybot

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   it "index return all posts" do
     get :index
-
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,17 @@
 FactoryBot.define do
+  factory :users  do
+    trait :ines do
+      username { Ines }
+      email { inesherald@gmail.com }
+      password  { 123456 }
+      password_confirmation { 123456 }
+    end
 
+    trait :checo do
+      username { checo }
+      email { checo@gmail.com }
+      password  { 123456 }
+      password_confirmation { 123456 }
+    end
+  end
 end
