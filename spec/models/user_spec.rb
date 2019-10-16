790517537192a00763e7ae962beacddd07486644
serdg0 <sergio.diazg92@gmail.com>
2019-10-16 13:05:10 -0500

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it "is invalid without a first name" do
-    user = User.new(username: nil)
-    user.valid?
+RSpec.describe User, :type => :model do
+
+  before(:all) do
+    @user1 = create(:user, :checo)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user1).to be_valid
+  end
+
+  it "has a unique username" do
+    user2 = build(:user, email: "bob@gmail.com")
+    expect(user2).to_not be_valid
+  end
+
+  it "has a unique email" do
+    user2 = build(:user, username: "Bob")
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user2 = build(:user, password: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a username" do
+    user2 = build(:user, username: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    user2 = build(:user, email: nil)
+    expect(user2).to_not be_valid
   end
 end
