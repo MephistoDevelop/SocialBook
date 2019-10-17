c51eb302755db1dc840c32faf71e74ec604eb968
Cristian <inesherald@gmail.com>
2019-10-17 12:47:58 -0500

    traits creations

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
 
+  before(:all) do
+    @user1 = create(:user, :checo)
+    visit('/users')
+    fill_in 'user_email', with: @user1.email
+    fill_in 'user_password', with: @user1.password
+    click_button 'Log in'
+    @post = create(:post, :event1)
+    @post.user_id=@user1.id
+  end
+
+  it "create a comment in a post" do
+    user = create(:user,:ines)
+    post = create(:post,:event1)
+    comment = user.comments.build(content:"This is my rspec test")
+    comment.post_id = post.id
+    comment.save
+    expect(comment).to be_valid
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -13,12 +13,37 @@ FactoryBot.define do
   end
 
   factory :user do
+
+    trait :controller_likes do
+      username { 'controller likes' }
+      email { 'controller_likes@mail.com' }
+      password { 123456 }
+      password_confirmation { 123456 }
+    end
+
+
+    trait :controller_post do
+      username { 'controller post' }
+      email { 'controller_post@mail.com' }
+      password { 123456 }
+      password_confirmation { 123456 }
+    end
+
+    trait :controller_comments do
+      username { 'controller comments' }
+      email { 'controller_comments@mail.com' }
+      password { 123456 }
+      password_confirmation { 123456 }
+    end
+
     trait :ines do
       username { 'Ines' }
-      email { 'inesherald@gmail.com' }
-      password { 123_456 }
-      password_confirmation { 123_456 }
+      email { 'ines@gmail.com' }
+      password { 123456 }
+      password_confirmation { 123456 }
     end
+  end
+
 
     trait :checo do
       username { 'checo' }
@@ -36,10 +61,27 @@ FactoryBot.define do
   end
 
   factory :post do
+
+    trait :post_user do
+      content { 'Event created from factoryBot post_users' }
+      user_id { 1 }
+    end
+
+    trait :post_comment do
+      content { 'Event created from factoryBot post_comments' }
+      user_id { 1 }
+    end
+
+    trait :post_likes do
+      content { 'Event created from factoryBot post_likes' }
+      user_id { 1 }
+    end
+
     trait :event1 do
       content { 'Event from factoryBot traits' }
       user_id { 1 }
     end
+
     trait :event2 do
       content { 'Event 2 from factoryBot traits' }
       user_id { 1 }
