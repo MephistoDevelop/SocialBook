d1e96c197aa50794d033e53a9631db8327e400ad
Cristian <inesherald@gmail.com>
2019-10-18 18:54:02 -0500

    Add fix views with helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module PostsHelper
+
+  def author_name(comment)
+    User.where(id: comment.author_id).pluck(:username).first
+  end
+
+  def post_author(post)
+    User.where(id: post.user_id).pluck(:username)[0]
+  end
+
+  def comment_author(comment)
+    User.where(id: comment.author_id).pluck(:username)[0]
+  end
 end
