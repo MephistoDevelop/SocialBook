8f7835df3e4f959a1349c549105e4cd8655ba0d4
Cristian <inesherald@gmail.com>
2019-11-01 17:56:33 -0600

    fix views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def friends_request_number
     current_user.friend_requests.size
   end
+
+  def set_name
+    if current_user.username != nil
+      return current_user.username
+    else
+      return current_user.name
+    end
+  end
 end
--- db/migrate/20191030233032_remove_columns_from_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveColumnsFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :provider, :string
-    remove_column :users, :uid, :string
-  end
-end
