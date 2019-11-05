98dab67bc2c0065f4a355a5592e3272fb324ba7e
Cristian <inesherald@gmail.com>
2019-11-05 16:02:04 -0600

    Add custom error pages

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,24 @@
+class ErrorsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def not_found
+    respond_to do |format|
+      format.html { render status: 404 }
+      format.json { render json: { error: "Resource not found" }, status: 404 }
+    end
+  end
+
+  def unacceptable
+    respond_to do |format|
+      format.html { render status: 422 }
+      format.json { render json: { error: "Params unacceptable" }, status: 422 }
+    end
+  end
+
+  def internal_error
+    respond_to do |format|
+      format.html { render status: 500 }
+      format.json { render json: { error: "Internal server error" }, status: 500 }
+    end
+  end
+end
---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ErrorsController, type: :controller do
+
+  describe "GET #not_found" do
+    it "returns http success" do
+      get :not_found
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #unacceptable" do
+    it "returns http success" do
+      get :unacceptable
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #internal_error" do
+    it "returns http success" do
+      get :internal_error
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
