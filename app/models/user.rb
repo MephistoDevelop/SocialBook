3e5cee1379924fcbec45959953007eb5b504b515
serdg0 <sergio.diazg92@gmail.com>
2019-11-05 15:03:16 -0600

    minor Oauth fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name # assuming the user model has a name
+      user.username = auth.info.name.split.first # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
     end
   end
