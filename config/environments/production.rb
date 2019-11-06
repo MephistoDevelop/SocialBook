8e18491f682fb12c3606985eccfe5f4a8f154f5d
serdg0 <sergio.diazg92@gmail.com>
2019-11-06 14:46:08 -0600

    added https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'socialbook2019.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://socialbook2019.herokuapp.com' }
 end
